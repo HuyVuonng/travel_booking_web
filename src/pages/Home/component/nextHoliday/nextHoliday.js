5a7a7f3963ad5f1813b2174c0a84a51ab0f903de
HuyVuong <vuonghuy12345@gmail.com>
2023-03-31 13:21:44 +0700

    done change picture

--- src/pages/Home/component/nextHoliday/nextHoliday.js
+++ src/pages/Home/component/nextHoliday/nextHoliday.js
@@ -7,68 +7,76 @@ const cx = classNames.bind(styles);
 function NextHoliday() {
     const picture = [
         {
-            id: 1,
+            id: 0,
             link: 'http://www.nicdarkthemes.com/themes/travel/wp/demo/love-travel/wp-content/uploads/sites/3/2018/11/package-3.jpg',
         },
         {
-            id: 2,
+            id: 1,
             link: 'http://www.nicdarkthemes.com/themes/travel/wp/demo/love-travel/wp-content/uploads/sites/3/2018/11/parallax-4.jpg',
         },
         {
-            id: 3,
+            id: 2,
             link: 'http://www.nicdarkthemes.com/themes/travel/wp/demo/love-travel/wp-content/uploads/sites/3/2018/11/parallax-4-1.jpg',
         },
     ];
-    const pictureLength = useRef(picture.length);
-    let timeoutid = useRef();
-    const btnchangepic = document.querySelectorAll('.change');
-    const [currentPicIndex, setCurrentPicIndex] = useState(0);
-    const [currentPic, setCurrentPic] = useState(picture[currentPicIndex]);
+    let idActivePic = useRef(0);
+    let IntervalID = useRef(0);
 
     const changePicAnimation = () => {
-        const pic = document.querySelector('.pic');
-
-        if (currentPicIndex < pictureLength.current - 1) {
-            setCurrentPicIndex(currentPicIndex + 1);
-        } else setCurrentPicIndex(0);
-
-        setCurrentPic(picture[currentPicIndex]);
-        for (var i = 0; i < btnchangepic.length; i++) {
-            btnchangepic[i].classList.remove(`${cx('active')}`);
-        }
-        for (var j = 0; j < btnchangepic.length; j++) {
-            if (btnchangepic[j].id - 1 === currentPicIndex) {
-                btnchangepic[j].classList.add(`${cx('active')}`);
-                pic.classList.add(`${cx('active-pic')}`);
+        IntervalID = setInterval(() => {
+            const picAll = document.querySelectorAll('.pic');
+            const btnchangepic = document.querySelectorAll('.change');
+            for (let i = 0; i < picAll.length; i++) {
+                if (picAll[i].classList.contains(`${cx('activePic')}`)) {
+                    idActivePic.current = picAll[i].dataset.id;
+                }
             }
-        }
-        setTimeout(() => {
-            pic.classList.remove(`${cx('active-pic')}`);
-        }, 8000);
+            let currentID = Number(idActivePic.current);
+            let nextID = currentID + 1;
+
+            if (nextID > picAll.length - 1) {
+                nextID = 0;
+            }
+            btnchangepic[currentID].classList.remove(`${cx('active')}`);
+            picAll[currentID].classList.remove(`${cx('activePic')}`);
+
+            idActivePic.current = nextID;
+
+            btnchangepic[nextID].classList.add(`${cx('active')}`);
+
+            for (let j = 0; j < picAll.length; j++) {
+                if (+picAll[j].dataset.id === nextID) {
+                    picAll[j].classList.add(`${cx('activePic')}`);
+                }
+            }
+        }, 5000);
     };
 
     useEffect(() => {
-        timeoutid.current = setTimeout(() => {
-            changePicAnimation();
-        }, 10000);
-        console.log('chayj');
-
-        return () => clearTimeout(timeoutid.current);
-    }, [currentPicIndex]);
-
-    const Changepicture = (id) => {
         const pic = document.querySelector('.pic');
         const btnchangepic = document.querySelectorAll('.change');
+        pic.classList.add(`${cx('activePic')}`);
+        btnchangepic[0].classList.add(`${cx('active')}`);
+        changePicAnimation();
+    }, []);
 
+    const Changepicture = (id) => {
+        const picAll = document.querySelectorAll('.pic');
+        const btnchangepic = document.querySelectorAll('.change');
         for (var i = 0; i < btnchangepic.length; i++) {
             btnchangepic[i].classList.remove(`${cx('active')}`);
+            picAll[i].classList.remove(`${cx('activePic')}`);
         }
 
-        btnchangepic[id - 1].classList.add(`${cx('active')}`);
-        setCurrentPicIndex(id - 1);
-        setCurrentPic(picture[currentPicIndex]);
-        clearTimeout(timeoutid.current);
-        pic.classList.add(`${cx('active-pic')}`);
+        btnchangepic[id].classList.add(`${cx('active')}`);
+        for (let i = 0; i < picAll.length; i++) {
+            if (+picAll[i].dataset.id === +id) {
+                picAll[i].classList.add(`${cx('activePic')}`);
+            }
+        }
+        clearInterval(IntervalID);
+        console.log('clear ' + IntervalID);
+        changePicAnimation();
     };
 
     return (
@@ -103,23 +111,21 @@ function NextHoliday() {
 
                 <div className={cx('nextHoliday-collum2')}>
                     <div className={cx('image-slide')}>
-                        <img className={cx('pic')} src={currentPic.link} alt="" />
+                        {/* <img className={cx('pic')} src={currentPic.link} alt="" /> */}
+                        {picture.map((img, index) => {
+                            return <img data-id={index} key={index} className={cx('pic')} src={img.link} alt="" />;
+                        })}
                         <div className={cx('change-pic-slide')}>
-                            <div
-                                id="1"
-                                onClick={() => Changepicture(1)}
-                                className={cx('btn-change-pic', 'active', 'change')}
-                            ></div>
-                            <div
-                                id="2"
-                                onClick={() => Changepicture(2)}
-                                className={cx('btn-change-pic', 'change')}
-                            ></div>
-                            <div
-                                id="3"
-                                onClick={() => Changepicture(3)}
-                                className={cx('btn-change-pic', 'change')}
-                            ></div>
+                            {picture.map((img) => {
+                                return (
+                                    <div
+                                        key={img.id}
+                                        id={img.id}
+                                        onClick={() => Changepicture(img.id)}
+                                        className={cx('btn-change-pic', 'change')}
+                                    ></div>
+                                );
+                            })}
                         </div>
                     </div>
                 </div>
